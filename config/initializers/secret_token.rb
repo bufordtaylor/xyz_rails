b7a54c80973efce769d8626853f510e2ce05e142
Christophe Maximin <christophe.maximin@gmail.com>
2014-04-02 02:22:00 +0100

    added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XyzRails::Application.config.secret_key_base = '97e4f207b223445250e88a5c4f87627f57fe7413f5de927cae33d5dfce58a18c926f757677983833b626d4e9a0c528d2909ea21c8f4314e6fb0005b2e7958712'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XyzRails::Application.config.session_store :cookie_store, key: '_xyz_rails_session'
